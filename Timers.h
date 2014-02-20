97e4910d9bd38cd4af01f1d3281a6c6ca9ffb0a3
JOSHUA DODGE WOODWARD <jdw6359@condor.(none)>
2014-02-19 21:46:35 -0500

    Starting to implement the timers module in timers.h, a macro based approach to implement timing

---
+++ Timers.h
@@ -0,0 +1,23 @@
+/*******************************************/
+/* Timers.h                                */
+/* Author: Greg Semeraro                   */
+/* Written By: Josh Woodward               */
+/* February 19, 2014                       */
+/* Timers module used as part of a         */
+/* performance measurement and modification*/
+/* assignment.                             */
+/*            				   */
+/*******************************************/
+
+#ifndef _TIMERS_H_
+#define _TIMERS_H_
+
+/*********************************************/
+/* The timers are enabled when the code      */
+/* is compiled. The following pre-processor  */
+/* code issues a warning and includes the    */
+/* necessary system header files when the    */
+/* timers are enabled                        */
+/*********************************************/
+
+
